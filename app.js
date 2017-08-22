29df8b1c6ab09993499c24d7c007d17e114739ff
Owner <Owner>
2017-08-22 16:39:20 -0500

    fixing end points, adding search

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ let path = require('path');
 let routes = require('./routes/index');
 let helpers = require('./helpers');
 
+let bodyParser = require('body-parser');
+
 let app = express();
 
 app.set('views', path.join(__dirname, '/views'));
@@ -11,6 +13,8 @@ app.set('view engine', 'pug');
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
 	res.locals.h = helpers;
--- routes/index.js
+++ routes/index.js
@@ -4,56 +4,80 @@ let router = express.Router();
 let unirest = require('unirest');
 
 router.get('/', (req, res) => {
-
 	let responseData;
 
-	unirest.get(`https://igdbcom-internet-game-database-v1.p.mashape.com/games/?fields=name,cover&order=popularity:desc`)
-		.header("X-Mashape-Key", "LuJ7CeWInTmsh2V727FmmsRAI9S1p1pYAbDjsnyd62PGDJbUQf")
-		.header("Accept", "application/json")
-		.end(function (result) {
-		  responseData = result.body;
+	unirest
+		.get(
+			`https://igdbcom-internet-game-database-v1.p.mashape.com/games/?fields=name,cover&order=popularity:desc`
+		)
+		.header(
+			'X-Mashape-Key',
+			'LuJ7CeWInTmsh2V727FmmsRAI9S1p1pYAbDjsnyd62PGDJbUQf'
+		)
+		.header('Accept', 'application/json')
+		.end(function(result) {
+			responseData = result.body;
 
-			res.render('index', { data: responseData, title: 'Home' })
+			res.render('index', { data: responseData, title: 'Home' });
 		});
-
-})
+});
 
 router.get('/upcoming', (req, res) => {
-
-	let date = new Date().getTime();
-
 	let responseData;
 
-	unirest.get(`https://igdbcom-internet-game-database-v1.p.mashape.com/release_dates/?fields=*&order=date:asc&filter[date][gt]=${date}`)
-		.header("X-Mashape-Key", "LuJ7CeWInTmsh2V727FmmsRAI9S1p1pYAbDjsnyd62PGDJbUQf")
-		.header("Accept", "application/json")
-		.end(function (result) {
+	unirest
+		.get(
+			`https://igdbcom-internet-game-database-v1.p.mashape.com/games/?fields=name,cover&order=release_dates.date:desc`
+		)
+		.header(
+			'X-Mashape-Key',
+			'LuJ7CeWInTmsh2V727FmmsRAI9S1p1pYAbDjsnyd62PGDJbUQf'
+		)
+		.header('Accept', 'application/json')
+		.end(function(result) {
+			responseData = result.body;
 
-			let list = [];
-			for (let i = 0; i < result.body.length; i++) {
-				list.push(result.body[i].id)
-			}
-			console.log(...list)
-
-			res.render('upcoming', { data: list, title: 'Upcoming Games' })
+			res.render('upcoming', { data: responseData, title: 'Upcoming Games' });
 		});
-
-})
+});
 
 router.get('/newreleases', (req, res) => {
-
-	let date = new Date().getTime();
-
 	let responseData;
 
-	unirest.get(`https://igdbcom-internet-game-database-v1.p.mashape.com/games/?filter[release_dates.date][lt]=${date}&fields=*&limit=20&offset=0&order=release_dates.date:desc:min`)
-		.header("X-Mashape-Key", "LuJ7CeWInTmsh2V727FmmsRAI9S1p1pYAbDjsnyd62PGDJbUQf")
-		.header("Accept", "application/json")
-		.end(function (result) {
-		  responseData = result.body;
+	unirest
+		.get(
+			`https://igdbcom-internet-game-database-v1.p.mashape.com/games/?fields=name,cover&order=release_dates.date:desc:min`
+		)
+		.header(
+			'X-Mashape-Key',
+			'LuJ7CeWInTmsh2V727FmmsRAI9S1p1pYAbDjsnyd62PGDJbUQf'
+		)
+		.header('Accept', 'application/json')
+		.end(function(result) {
+			responseData = result.body;
 
-			res.render('newReleases', { data: responseData, title: 'New Releases' })
+			res.render('newReleases', { data: responseData, title: 'New Releases' });
 		});
-})
+});
 
-module.exports = router
+router.post('/search', (req, res) => {
+	let responseData;
+	let gameTitle = req.body.title.split(' ').join('+');
+
+	unirest
+		.get(
+			`https://igdbcom-internet-game-database-v1.p.mashape.com/games/?search=${gameTitle}&fields=name,cover&order=release_dates.date:desc:min&limit=20`
+		)
+		.header(
+			'X-Mashape-Key',
+			'LuJ7CeWInTmsh2V727FmmsRAI9S1p1pYAbDjsnyd62PGDJbUQf'
+		)
+		.header('Accept', 'application/json')
+		.end(function(result) {
+			responseData = result.body;
+
+			res.render('search', { data: responseData, title: 'New Releases' });
+		});
+});
+
+module.exports = router;
