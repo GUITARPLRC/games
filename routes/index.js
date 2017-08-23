bbf32b4be7995f0115d4b186f823bff27b161f55
guitarplrc <guitarplrc@yahoo.com>
2017-08-22 19:10:19 -0500

    added individual game route

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ router.get('/', (req, res) => {
 
 	unirest
 		.get(
-			`https://igdbcom-internet-game-database-v1.p.mashape.com/games/?fields=name,cover&order=popularity:desc`
+			`https://igdbcom-internet-game-database-v1.p.mashape.com/games/?fields=name,cover,summary&order=popularity:desc`
 		)
 		.header(
 			'X-Mashape-Key',
@@ -28,7 +28,7 @@ router.get('/upcoming', (req, res) => {
 
 	unirest
 		.get(
-			`https://igdbcom-internet-game-database-v1.p.mashape.com/games/?fields=name,cover&filter=[first_release_date][gte]=${date}&order=release_dates.date:desc`
+			`https://igdbcom-internet-game-database-v1.p.mashape.com/games/?fields=name,cover,summary&filter=[first_release_date][gt]=${date}&order=first_release_date:desc&limit=20`
 		)
 		.header(
 			'X-Mashape-Key',
@@ -43,11 +43,12 @@ router.get('/upcoming', (req, res) => {
 });
 
 router.get('/newreleases', (req, res) => {
+	let date = new Date();
 	let responseData;
 
 	unirest
 		.get(
-			`https://igdbcom-internet-game-database-v1.p.mashape.com/games/?fields=name,cover&order=release_dates.date:desc:min`
+			`https://igdbcom-internet-game-database-v1.p.mashape.com/games/?fields=name,cover,summary&filter=[first_release_date][lt]=${date}&order=first_release_date:desc&limit=20`
 		)
 		.header(
 			'X-Mashape-Key',
@@ -61,13 +62,13 @@ router.get('/newreleases', (req, res) => {
 		});
 });
 
-router.post('/search', (req, res) => {
+router.get('/search', (req, res) => {
 	let responseData;
-	let gameTitle = req.body.title.split(' ').join('+');
+	let gameTitle = req.query.title.split(' ').join('+');
 
 	unirest
 		.get(
-			`https://igdbcom-internet-game-database-v1.p.mashape.com/games/?search=${gameTitle}&fields=name,cover&limit=20`
+			`https://igdbcom-internet-game-database-v1.p.mashape.com/games/?search=${gameTitle}&fields=name,cover,summary,id&limit=20`
 		)
 		.header(
 			'X-Mashape-Key',
@@ -77,7 +78,27 @@ router.post('/search', (req, res) => {
 		.end(function(result) {
 			responseData = result.body;
 
-			res.render('search', { data: responseData, title: 'New Releases' });
+			res.render('search', { data: responseData, title: 'Search' });
+		});
+});
+
+router.get('/:id', (req, res) => {
+	let gameId = req.params.id;
+	let responseData;
+
+	unirest
+		.get(
+			`https://igdbcom-internet-game-database-v1.p.mashape.com/games/${gameId}?fields=*`
+		)
+		.header(
+			'X-Mashape-Key',
+			'LuJ7CeWInTmsh2V727FmmsRAI9S1p1pYAbDjsnyd62PGDJbUQf'
+		)
+		.header('Accept', 'application/json')
+		.end(function(result) {
+			responseData = result.body;
+
+			res.render('game', { data: responseData, title: `${req.params.id}` });
 		});
 });
 
